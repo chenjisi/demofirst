c21b3cd47a3ca51b2798f404d9a60fd807f45150
cjs <526424978@qq.com>
2021-08-23 12:53:35 +0800

    首次提交

---
+++ src/Test.java
@@ -0,0 +1,20 @@
+/**
+ * The type Test. @author cjs
+ *
+ * @version 1.0.0
+ * @since 2021年8月23日 下午12:39:12
+ */
+public class Test {
+
+  /**
+   * The entry point of application.
+   *
+   * @author cjs
+   * @version 1.0.0
+   * @since 2021年8月23日 下午12:39:12
+   * @param args the input arguments
+   */
+  public static void main(String[] args) {
+    //
+  }
+}
